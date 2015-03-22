a870fe8a9b7d60a7b1a3c527f3abad756bd63e01
elklein96 <elklein96@gmail.com>
2015-03-22 14:44:24 -0400

    Fix mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "/mail/contact_me.php",
+                url: "./mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
